2b62d62e1c55206e43a7afd8b234e01641da6c28
Ricky Walsh <rickymwalsh@gmail.com>
2020-11-11 18:26:16 +0100

    Added precision evaluation

--- Project/evaluate.py
+++ Project/evaluate.py
@@ -1,4 +1,6 @@
 import pandas as pd
+from evaluation_metrics import precision_at_k, mean_average_precision, sign_test_values
+
 
 df = pd.read_excel('runs/Run1.xlsx')
 threshold = 22
@@ -13,3 +15,24 @@ print(f'Overall negatives: {TN + FP}')
 
 print(f'Confusion matrix:')
 print(f'{TP:<5} | {FN:<5}\n{FP:<5} | {TN:<5}')
+
+
+# Find queries with relevant images.
+queries_with_relevant = df.groupby('query_index').filter(lambda x: x['similar'].max() == 1)
+query_indices = set(queries_with_relevant['query_index'].tolist())
+
+all_relevant = {}
+all_retrieved = {}
+precision_at_10 = {}
+
+for ind in query_indices:
+    retrieved = df[df['query_index'] == ind].map_index.tolist()
+    relevant = df[(df['query_index'] == ind) & (df['similar'] == 1)].map_index.tolist()
+    
+    all_relevant[ind] = relevant
+    all_retrieved[ind] = retrieved
+    
+    precision_at_10[ind] = precision_at_k(relevant, retrieved, 10)
+
+print(mean_average_precision(all_relevant, all_retrieved))
+print("Precision at 10:", precision_at_10)
